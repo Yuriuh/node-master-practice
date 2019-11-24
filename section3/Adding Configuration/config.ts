8b59f5fda41ada9a1d6fd1439b53c1ab0fd1abc7
yuriuh <642192014@qq.com>
2019-11-24 13:12:22 +0800

    learn adding configuration

---
+++ section3/Adding Configuration/config.ts
@@ -0,0 +1,36 @@
+// Container for all environments
+interface Config {
+  port: number
+  envName: string
+}
+interface Enviroments {
+  staging: Config
+  production: Config
+}
+
+// Container for all environments
+const environments = {} as Enviroments
+
+// Staging (default) environment
+environments.staging = {
+  'port': 3000,
+  'envName': 'staging'
+}
+
+// Production environment
+environments.production = {
+  'port': 5000,
+  'envName': 'production'
+}
+
+// Determine which environment was passed as a command-line argument
+const currentEnvironment: string = typeof(process.env.NODE_ENV) === 'string'
+  ? process.env.NODE_ENV.toLowerCase()
+  : ''
+
+// Check that the current environment is one of the environments above, if not default to staging
+const environmentToExport = typeof(environments[currentEnvironment]) === 'object'
+  ? environments[currentEnvironment]
+  : environments.staging
+
+export const config = environmentToExport
