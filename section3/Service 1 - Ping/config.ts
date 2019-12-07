e862c8a0fdeaddf1889d9a32aab2ff03e6ab3be1
yuriuh <642192014@qq.com>
2019-12-08 00:04:34 +0800

    add https

---
+++ section3/Service 1 - Ping/config.ts
@@ -0,0 +1,40 @@
+// Container for all environments
+interface Config {
+  httpPort: number
+  httpsPort: number
+  envName: string
+}
+interface Enviroments {
+  staging: Config
+  production: Config
+}
+
+// Container for all environments
+const environments = {} as Enviroments
+
+// Staging (default) environment
+environments.staging = {
+  'httpPort': 3000,
+  'httpsPort': 3001,
+  'envName': 'staging'
+}
+
+// Production environment
+environments.production = {
+  'httpPort': 5000,
+  'httpsPort': 5001,
+  'envName': 'production'
+}
+
+// Determine which environment was passed as a command-line argument
+const currentEnvironment: string = typeof (process.env.NODE_ENV) === 'string'
+  ? process.env.NODE_ENV.toLowerCase()
+  : ''
+
+// Check that the current environment is one of the environments above, if not default to staging
+const environmentToExport = typeof (environments[currentEnvironment]) === 'object'
+  ? environments[currentEnvironment]
+  : environments.staging
+
+// export const config = environmentToExport
+export default environmentToExport
---
+++ section3/Service 1 - Ping/index.ts
@@ -0,0 +1,117 @@
+// Dependencies
+import * as http from 'http'
+import * as https from 'https'
+import * as url from 'url'
+import * as fs from 'fs'
+import { StringDecoder } from 'string_decoder'
+import config from './config'
+
+// Instantiate the HTTP server
+const httpServer = http.createServer((request, response) => {
+  unifiedServer(request, response)
+})
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, () => {
+  console.log('The server is up and running now')
+  console.log('port', config.httpPort)
+})
+
+// Instantiate the HTTPS server
+const httpsServerOptions = {
+  'key': fs.readFileSync(__dirname + '/https/key.pem'),
+  'cert': fs.readFileSync(__dirname + '/https/cert.pem')
+}
+const httpsServer = https.createServer(httpsServerOptions, (request, response) => {
+  unifiedServer(request, response)
+})
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+  console.log('The server is up and running now')
+  console.log('port', config.httpsPort)
+})
+
+// All the server logic for both the http and https server
+const unifiedServer = (request, response) => {
+  // Parse the url
+  const parsedUrl = url.parse(request.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query
+
+  // Get the HTTP method
+  const method = request.method.toLowerCase()
+
+  // Get the headers as an object
+  const headers = request.headers
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  request.on('data', data => {
+    buffer += decoder.write(data)
+  })
+  request.on('end', () => {
+    buffer += decoder.end()
+
+    // Check the router for a matching path for a handler. If one is not found,
+    // use the notFound handler instead.
+    const chosenHandler = typeof (router[trimmedPath]) !== 'undefined'
+      ? router[trimmedPath]
+      : handlers.notFound
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer
+    }
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the status code returned form the handler, or set the default status code to 200
+      statusCode = typeof (statusCode) === 'number' ? statusCode : 200
+
+      // Use the payload returned from the handler, or set the default payload to an empty object
+      payload = typeof (payload) === 'object' ? payload : {}
+
+      // Convert the payload to a string
+      const payloadString = JSON.stringify(payload)
+
+      // Return the response
+      response.setHeader('Content-Type', 'application/json')
+      response.writeHead(statusCode)
+      response.end(payloadString)
+      console.log('Returning this response: ', statusCode, payloadString)
+    })
+  })
+}
+
+interface Handlers {
+  ping: any,
+  notFound: any
+}
+// Define all the handlers
+const handlers = {} as Handlers
+
+// Ping handler
+handlers.ping = function (data, callback) {
+  callback(200)
+}
+
+// Not found handler
+handlers.notFound = function (data, callback) {
+  callback(404)
+}
+
+// Define the request router
+const router = {
+  'ping': handlers.ping
+}
